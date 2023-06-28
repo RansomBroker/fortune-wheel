63bc835bf8999be21126b0e118f4e7ae1e7415c0
RansomBroker <yadisoke@gmail.com>
2023-06-28 22:34:12 +0700

    perubahan db

--- fortunewheel_db.sql
+++ fortunewheel_db.sql
@@ -1,85 +1,63 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: localhost    Database: fortunewheel_db
--- ------------------------------------------------------
--- Server version	8.0.32
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : lnpp-8-mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50722
+ Source Host           : 17.17.17.5:3306
+ Source Schema         : fortunewheel_db
 
---
--- Table structure for table `labels`
---
+ Target Server Type    : MySQL
+ Target Server Version : 50722
+ File Encoding         : 65001
 
+ Date: 28/06/2023 22:33:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for labels
+-- ----------------------------
 DROP TABLE IF EXISTS `labels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `labels` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `fillStyle` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `img` varchar(255) NOT NULL,
-  `counter` bigint NOT NULL,
-  `is_prize` smallint NOT NULL DEFAULT '0',
+  `counter` bigint(20) NOT NULL,
+  `is_prize` smallint(6) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `labels`
---
+-- ----------------------------
+-- Records of labels
+-- ----------------------------
+BEGIN;
+COMMIT;
 
-LOCK TABLES `labels` WRITE;
-/*!40000 ALTER TABLE `labels` DISABLE KEYS */;
-INSERT INTO `labels` VALUES (17,'#5dc1d8','Ipong\nYang\nMahal','1687937476.jpeg',1,1),(18,'#f42d92','Rice\nKuker\nMahal','1687937497.jpeg',2,1),(19,'#5dc1d8','Bitkoin','1687937523.png',3,1),(20,'#f42d92','Ipet\nMini','1687937542.jpeg',4,1);
-/*!40000 ALTER TABLE `labels` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `settings`
---
-
+-- ----------------------------
+-- Table structure for settings
+-- ----------------------------
 DROP TABLE IF EXISTS `settings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `settings` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `bg_img` varchar(255) DEFAULT NULL,
   `logo_img` varchar(255) DEFAULT NULL,
   `header_img` varchar(255) DEFAULT NULL,
   `body_img` varchar(255) DEFAULT NULL,
   `footer_img` varchar(255) DEFAULT NULL,
-  `setting_id` bigint NOT NULL,
+  `setting_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `setting_id` (`setting_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `settings`
---
+-- ----------------------------
+-- Records of settings
+-- ----------------------------
+BEGIN;
+INSERT INTO `settings` VALUES (1, 'background-default.png', 'logo-default.png', 'header-default.png', 'body-default.png', 'footer-default.png', 1);
+COMMIT;
 
-LOCK TABLES `settings` WRITE;
-/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
-INSERT INTO `settings` VALUES (1,'background-default.png','logo-default.png','header-default.png','body-default.png','footer-default.png',1);
-/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-28 14:54:03
+SET FOREIGN_KEY_CHECKS = 1;
